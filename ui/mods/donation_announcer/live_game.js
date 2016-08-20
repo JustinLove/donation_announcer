88612ba683a908702d2c4179fb48198a2695a035
Justin Love <git@JustinLove.name>
2016-08-19 19:58:42 -0500

    make more tolerant of running in start

--- ui/mods/donation_announcer/live_game.js
+++ ui/mods/donation_announcer/live_game.js
@@ -4,29 +4,33 @@
   paths.sandbox_unit_menu = 'coui://ui/mods/sandbox_unit_menu'
 
   handlers.request_player_names = function(payload) {
-    //api.Panel.message(payload[0], payload[1],
-      //model.playerData().names)
+    if (model.playerData) {
+      api.Panel.message(payload[0], payload[1],
+        model.playerData().names)
+    }
     api.panels.options_bar && 
       api.panels.options_bar.message('donation_announcer_loading', false)
   }
-  /*
   handlers.request_planet_names = function(payload) {
-    api.Panel.message(payload[0], payload[1],
-      model.celestialViewModels().map(function(planet) {return planet.name()}))
+    if (model.celestialViewModels) {
+      api.Panel.message(payload[0], payload[1],
+        model.celestialViewModels().map(function(planet) {return planet.name()}))
+    }
   }
-  */
 })()
 
 require(['donation_announcer/panel'], function(panel) {
   panel.show()
 
-  /*
-  model.playerData.subscribe(function(value) {
-    api.Panel.message('donation_announcer', 'player_names', value.names)
-  })
-  model.celestialViewModels.subscribe(function(value) {
-    api.Panel.message('donation_announcer', 'planet_names',
-      value.map(function(planet) {return planet.name()}))
-  })
-  */
+  if (model.playerData) {
+    model.playerData.subscribe(function(value) {
+      api.Panel.message('donation_announcer', 'player_names', value.names)
+    })
+  }
+  if (model.celestialViewModels) {
+    model.celestialViewModels.subscribe(function(value) {
+      api.Panel.message('donation_announcer', 'planet_names',
+        value.map(function(planet) {return planet.name()}))
+    })
+  }
 })
